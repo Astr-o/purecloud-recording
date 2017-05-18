61af44d7d60fda600f014712d90c510976881211
Kroum Klutchkov <kroum.klutchkov@employsure.com.au>
2017-05-18 10:06:32 +1000

    Adds getUrisSeperateChannels function

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const path = require('path')
 
-
 // external modules
 const purecloud = require('purecloud_api_sdk_javascript')
 
@@ -8,6 +7,10 @@ const purecloud = require('purecloud_api_sdk_javascript')
 const api = require('./lib/recording-api')
 const downloadFile = require('./lib/download-file')
 const utils = require('./lib/utils')
+
+const DEFAULT_RETRY_INTERVAL = 60 * 1000 // 60 secs
+const DEFAULT_RETRYS = 3
+const DEFAULT_FORMAT_ID = 'WAV'
 
 module.exports = class CallRecordingDownloader {
     constructor(session) {
@@ -20,10 +23,12 @@ module.exports = class CallRecordingDownloader {
      * @param {string} interactionId 
      * @param {string} outputPath 
      * @param {string} formatId 
-     * @param {number} retrys 
-     * @param {number} retryInterval 
+     * @param {number} retrys - optional
+     * @param {number} retryInterval - optional
+     * 
+     * @returns {Promise<[string]>} - path on disk where it was downloaded
      */
-    downloadSeperateChannels(interactionId, outputPath, formatId, retrys, retryInterval) {
+    downloadSeperateChannels(interactionId, outputPath, formatId = DEFAULT_FORMAT_ID, retrys = DEFAULT_RETRYS, retryInterval = DEFAULT_RETRY_INTERVAL) {
         return api.getCallRecordingMetadata(this.recordingApi, interactionId, formatId, retrys, retryInterval)
             .then(
                 function downloadCallRecordings(metadata) {
@@ -42,6 +47,31 @@ module.exports = class CallRecordingDownloader {
                         downloadFile(uris[0], file0Path),
                         downloadFile(uris[1], file1Path)
                     ])
+                }
+            )
+    }
+
+    /**
+     * Gets the uris of the two audio files associated with the interactions, returns uris in an array
+     * 
+     * @param {string} interactionId 
+     * @param {string} formatId 
+     * @param {number} retrys - optional
+     * @param {number} retryInterval - optional
+     * 
+     * @returns {Promise<[string]>} - uris of both recordings
+     */
+    getUrisSeperateChannels(interactionId, formatId = DEFAULT_FORMAT_ID, retrys = DEFAULT_RETRYS, retryInterval = DEFAULT_RETRY_INTERVAL) {
+        return api.getCallRecordingMetadata(this.recordingApi, interactionId, formatId, retrys, retryInterval)
+            .then(
+                function getUris(metadata) {
+                    if (!metadata.mediaUris) {
+                        return Promise.reject(new Error('RecordingUriNotFound'))
+                    }
+
+                    const uris = [metadata.mediaUris[0].mediaUri, metadata.mediaUris[1].mediaUri]
+
+                    return Promise.resolve(uris)
                 }
             )
     }
